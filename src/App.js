f501af38417508b7ff8f9a7b8c95429782fffd12
Laura Marques <84184642+lauradorock@users.noreply.github.com>
2023-05-30 11:58:46 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import Header from "./components/Header";
+import "./App.css";
+
+function App() { 
+  
+  function reproduzVideo(onclick) {
+      alert("O vídeo está sendo reproduzido")
+  }
+
+  return (
+    <>
+  <body>
+   
+      <Header/>
+        <main>
+            <nav class="menu-vertical">
+                <ul>
+                    <li class="botoes-meunu-vertical">Início</li>
+                    <li class="botoes-meunu-vertical">Em alta</li>
+                    <li class="botoes-meunu-vertical">Inscrições</li>
+                    <hr></hr>
+                    <li class="botoes-meunu-vertical">Originais</li>
+                    <li class="botoes-meunu-vertical">Histórico</li>
+                </ul>
+            </nav>
+
+            <section class="painel-de-videos">
+                <div class="box-pagina-principal media1" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=1 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div class="box-pagina-principal media2" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=2 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div class="box-pagina-principal media3" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=3 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div class="box-pagina-principal media4" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=4 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div class="box-pagina-principal media5" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=5 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div class="box-pagina-principal media6" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=6 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div class="box-pagina-principal media7" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=7 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+                <div class="box-pagina-principal media8" onclick={reproduzVideo}>
+                    <img src="https://picsum.photos/400/400?a=8 " alt=""></img>
+                    <h4>Título do vídeo</h4>
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <h4>Oi! Eu moro no footer!</h4>
+        </footer>
+</body>
+    </>  
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function Header(){
+    return(
+        <header>
+            <h1>video</h1>
+            <input type="text" placeholder="Busca" id="Campo de Busca"></input>
+        </header>
+    )
+}
+export default Header
